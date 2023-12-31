05263adb6ffecd833c5c49ed7a7f586cd32bf9f8
sdhaduk <sagardhaduk1@gmail.com>
2023-12-31 17:44:46 -0600

    added ThemeProvider to Providers

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,12 +1,21 @@
-import { SessionProvider } from "next-auth/react";
+"use client";
+
 import React from "react";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
-type Props = {
-  children: React.ReactNode;
-};
-
-const Providers = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+const Providers = ({ children, ...props }: ThemeProviderProps) => {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      {...props}
+    >
+      <SessionProvider>{children}</SessionProvider>
+    </NextThemesProvider>
+  );
 };
 
 export default Providers;
