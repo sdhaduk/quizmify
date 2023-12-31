db8f22f8b5aee05ee7d9a637a44c4c165a3577fb
sdhaduk <sagardhaduk1@gmail.com>
2023-12-31 17:26:13 -0600

    added dropdown menu to navbar that displays user info

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,12 @@ import { getAuthSession } from "@/lib/nextauth";
 import React from "react";
 import Link from "next/link";
 import SignInButton from "./SignInButton";
+import UserAccountNav from "./UserAccountNav";
 
 type Props = {};
 
 const Navbar = async (props: Props) => {
   const session = await getAuthSession();
-
   return (
     <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
       <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
@@ -20,7 +20,7 @@ const Navbar = async (props: Props) => {
 
         <div className="flex items-center">
           {session?.user ? (
-            <h1>welcome {session.user.name}</h1>
+            <UserAccountNav user={session.user}/>
           ) : (
             <SignInButton text="Sign In" />
           )}
