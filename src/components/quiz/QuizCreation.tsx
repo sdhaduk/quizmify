037548860175a2321334b6fa8a58cf4ed31164c5
sdhaduk <sagardhaduk1@gmail.com>
2024-01-01 00:03:05 -0600

    created QuizCreation page that displays form

---
+++ src/components/quiz/QuizCreation.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { useForm } from "react-hook-form";
+import { quizCreationSchema } from "@/schemas/form/quiz";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Separator } from "../ui/separator";
+
+type Props = {};
+type Input = z.infer<typeof quizCreationSchema>;
+
+const QuizCreation = (props: Props) => {
+  const form = useForm<Input>({
+    resolver: zodResolver(quizCreationSchema),
+    defaultValues: {
+      amount: 3,
+      topic: "",
+      type: "open-ended",
+    },
+  });
+
+  const onSubmit = (input: Input) => {
+    alert(JSON.stringify(input, null));
+  };
+
+  form.watch();
+
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">Quiz Creation</CardTitle>
+          <CardDescription>Choose a topic</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="topic"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Topic</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter a topic" {...field} />
+                    </FormControl>
+                    <FormDescription>Please provide a topic</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Number of Questions</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter an amount"
+                        {...field}
+                        type="number"
+                        min={1}
+                        max={10}
+                        onChange={(e) => {
+                          form.setValue("amount", parseInt(e.target.value));
+                        }}
+                      />
+                    </FormControl>
+                    <FormDescription>Please </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between">
+                <Button
+                  type="button"
+                  onClick={() => {
+                    form.setValue("type", "mcq");
+                  }}
+                  className="w-1/2 rounded-none rounded-l-lg"
+                  variant={
+                    form.getValues("type") === "mcq" ? "default" : "secondary"
+                  }
+                >
+                  Mulitple Choice
+                </Button>
+                <Separator orientation="vertical" />
+                <Button
+                  type="button"
+                  onClick={() => {
+                    form.setValue("type", "open-ended");
+                  }}
+                  className="w-1/2 rounded-none rounded-r-lg"
+                  variant={
+                    form.getValues("type") === "open-ended"
+                      ? "default"
+                      : "secondary"
+                  }
+                >
+                  Open Ended
+                </Button>
+              </div>
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default QuizCreation;
