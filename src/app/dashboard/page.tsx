0be6ae6b82cde075305768a0e4327610035f01ae
sdhaduk <sagardhaduk1@gmail.com>
2023-12-31 22:16:17 -0600

    added card components to dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,40 @@
+import HistoryCard from "@/components/dashboard/HistoryCard";
+import HotTopicsCard from "@/components/dashboard/HotTopicsCard";
+import QuizMeCard from "@/components/dashboard/QuizMeCard";
+import RecentActivity from "@/components/dashboard/RecentActivity";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {};
+
+export const metadata = {
+  title: "Dashboard | Quizmify",
+};
+
+const page = async (props: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    redirect("/");
+  }
+
+  return (
+    <main className="p-8 mx-auto max-w-7xl ">
+      <div className="flex items-center">
+        <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
+      </div>
+
+      <div className="grid gap-4 mt-4 md:grid-cols-2">
+        <QuizMeCard />
+        <HistoryCard />
+      </div>
+
+      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
+        <HotTopicsCard />
+        <RecentActivity />
+      </div>
+    </main>
+  );
+};
+
+export default page;
