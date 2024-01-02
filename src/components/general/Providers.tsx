73ce4e1a859e7bdf2f3c06498096dd560beae9a2
sdhaduk <sagardhaduk1@gmail.com>
2024-01-01 20:03:33 -0600

    added QueryClient for useMutation

--- src/components/general/Providers.tsx
+++ src/components/general/Providers.tsx
@@ -4,17 +4,22 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const Providers = ({ children, ...props }: ThemeProviderProps) => {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      {...props}
-    >
-      <SessionProvider>{children}</SessionProvider>
-    </NextThemesProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        {...props}
+      >
+        <SessionProvider>{children}</SessionProvider>
+      </NextThemesProvider>
+    </QueryClientProvider>
   );
 };
 
