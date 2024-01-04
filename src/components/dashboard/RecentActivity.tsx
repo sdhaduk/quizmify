9f9b4ed862b2388dd05978d1075e70b286062a27
sdhaduk <sagardhaduk1@gmail.com>
2024-01-04 02:44:16 -0600

    finished component to display 10 most recent games

--- src/components/dashboard/RecentActivity.tsx
+++ src/components/dashboard/RecentActivity.tsx
@@ -6,21 +6,37 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
+import HistoryComponent from "./HistoryComponent";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
+import { prisma } from "@/lib/db";
 
 type Props = {};
 
-const RecentActivity = (props: Props) => {
+const RecentActivity = async (props: Props) => {
+  const session = await getAuthSession();
+
+  if (!session?.user) {
+    return redirect("/");
+  }
+
+  const gamesCount = await prisma.game.count({
+    where: { userId: session.user.id },
+  });
+
   return (
     <Card className="col-span-7">
       <CardHeader>
         <CardTitle className="text-2xl font-bold">Recent Activity</CardTitle>
-        <CardDescription>You have a played a total of 7 games</CardDescription>
+        <CardDescription>
+          You have a played a total of {gamesCount} games
+        </CardDescription>
       </CardHeader>
       <CardContent className="max-h-[580px] overflow-scroll">
-        recents
+        <HistoryComponent limit={10} userId={session.user.id} />
       </CardContent>
     </Card>
   );
-}; 
+};
 
 export default RecentActivity;
