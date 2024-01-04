9a187cd4e90bcc0447760907d13cf00a4987b725
sdhaduk <sagardhaduk1@gmail.com>
2024-01-04 02:43:28 -0600

    finished history page

---
+++ src/app/history/page.tsx
@@ -0,0 +1,37 @@
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import { LucideLayoutDashboard } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import HistoryComponent from "@/components/dashboard/HistoryComponent";
+
+type Props = {};
+
+const page = async (props: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect("/");
+  }
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[450px]">
+      <Card>
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <CardTitle className="text-2xl font-bold">History</CardTitle>
+            <Link href="/dashboard" className={buttonVariants()}>
+              <LucideLayoutDashboard className="mr-2" />
+              Back to Dashboard
+            </Link>
+          </div>
+        </CardHeader>
+        <CardContent className="max-h-[60vh] overflow-scroll ">
+          <HistoryComponent limit={100} userId={session.user.id} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default page;
