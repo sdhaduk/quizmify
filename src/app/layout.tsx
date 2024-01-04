57f4c8344b9d81bc0c369b5241e04728335ebb15
sdhaduk <sagardhaduk1@gmail.com>
2024-01-03 19:03:46 -0600

    added toast to src layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/nav/Navbar";
 import Providers from "@/components/general/Providers";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           {children}
+          <Toaster />
         </Providers>
       </body>
     </html>
