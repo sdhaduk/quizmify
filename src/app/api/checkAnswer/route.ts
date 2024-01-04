afe203991db8a4dcf5dd69f83f97164ad5f63bbd
sdhaduk <sagardhaduk1@gmail.com>
2024-01-03 21:24:07 -0600

    updated route to be able to handle open_ended question types

--- src/app/api/checkAnswer/route.ts
+++ src/app/api/checkAnswer/route.ts
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/db";
 import { checkAnswerSchema } from "@/schemas/form/quiz";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
+import { compareTwoStrings } from "string-similarity";
 
 export const POST = async (req: Request, res: Response) => {
   try {
@@ -49,9 +50,27 @@ export const POST = async (req: Request, res: Response) => {
           status: 200,
         }
       );
-    }
+    } else if (question.questionType === "open_ended") {
+      let percentageSimiliar = compareTwoStrings(
+        userAnswer.toLowerCase().trim(),
+        question.answer.toLowerCase().trim()
+      );
+      percentageSimiliar = Math.round(percentageSimiliar * 100);
 
-     
+      await prisma.question.update({
+        where: { id: questionId },
+        data: {
+          percentageCorrect: percentageSimiliar,
+        },
+      });
+      
+      return NextResponse.json(
+        {
+          percentageSimiliar,
+        },
+        { status: 200 }
+      );
+    }
   } catch (error) {
     if (error instanceof ZodError) {
       return NextResponse.json(
