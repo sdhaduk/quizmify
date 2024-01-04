9912c6d5df51a95e83321dc7b3710536f1fe6872
sdhaduk <sagardhaduk1@gmail.com>
2024-01-04 16:48:46 -0600

    fixed bug that did not display userAnswer in statistics page

--- src/components/game/OpenEnded.tsx
+++ src/components/game/OpenEnded.tsx
@@ -36,7 +36,7 @@ const OpenEnded = ({ game }: Props) => {
     mutationFn: async () => {
       let filledAnswer = blankAnswer;
       document.querySelectorAll("#user-blank-input").forEach((input: any) => {
-        filledAnswer = filledAnswer.replace("-----", input.value);
+        filledAnswer = filledAnswer.replace("_____", input.value);
         input.value = "";
       });
       const payload: z.infer<typeof checkAnswerSchema> = {
